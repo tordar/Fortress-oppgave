a8ce54469e3080d5d62785cc91160e003318bebb
tordar <tordar.tommervik@gmail.com>
2021-09-17 20:14:05 +0200

    first 2 colum layout react

--- fortress/src/App.js
+++ fortress/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="row">
+        <div className="column">
+          <div className="whiteCol">
+            <SignIn />
+          </div>
+        </div>
+        <div className="column">
+          <div className="blueCol">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ fortress/src/components/SignIn.js
@@ -0,0 +1,12 @@
+import Bolt from '../img/bolt.png'
+
+export const SignIn = () => {
+     
+    return (
+        <div>
+            <img id="boltLogo" src={Bolt} alt=""></img>
+        </div>
+    )
+}
+
+export default SignIn
