6a1c35660e8622c9511f0f8c945f1f2ffa36a21f
tordar <tordar.tommervik@gmail.com>
2021-09-18 15:29:21 +0200

    app fix

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <Router>
     <div className="App">
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/login" />
+        <Route path="/">
+          <LogIn />
         </Route>
         <Route path="/login">
           <LogIn />
