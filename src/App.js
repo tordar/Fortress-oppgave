a3f1fe6153e1c2ed9d3fcd908f851d2fa15cddf0
tordar <tordar.tommervik@gmail.com>
2021-09-18 15:56:02 +0200

    trying something different

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
     <div className="App">
+
+   
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+
+        <hr />
+    
       <Switch>
-        <Route exact path="/" >
+        {/* <Route exact path="/" >
           <Redirect to="/login" />
-        </Route>
+        </Route> */}
         <Route exact path="/login" >
           <LogIn />
         </Route>
